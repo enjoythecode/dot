cdc8c1b0ddf841165768e18585d004c9b2a710ee
Sinan Yumurtacı <snny@google.com>
2022-07-01 17:08:06 +0000

    sh/link.sh: create ~/.config/nvim if not exists

--- sh/link.sh
+++ sh/link.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ln -s ~/dot/vim/.vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
 ln -s ~/dot/nvim/init.vim ~/.config/nvim/init.vim
 echo "[ -f $HOME/dot/bash/entry.bashrc ] && source $HOME/dot/bash/entry.bashrc" >> ~/.bashrc
