51245eb1a0ff3d5c18c03057697c87407a8adc5f
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-07-01 00:51:55 -0700

    move nvim config to this repo

--- sh/link.sh
+++ sh/link.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ln -s ~/dot/vim/.vimrc ~/.vimrc
+ln -s ~/dot/nvim/init.vim ~/.config/nvim/init.vim
 echo "[ -f $HOME/dot/bash/entry.bashrc ] && source $HOME/dot/bash/entry.bashrc" >> ~/.bashrc
