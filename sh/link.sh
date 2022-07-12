0d0f2b9c9d857513a516f8237fc5b8d7b11bf4ec
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-07-11 20:37:47 -0700

    link ~/.profile to ~/.bashrc on OSX
    because on OSX, ~/.bashrc is not automatically read, but ~/.profile is

--- sh/link.sh
+++ sh/link.sh
@@ -3,3 +3,8 @@
 ln -s ~/dot/nvim ~/.config/
 ln -s ~/dot/sinany/docs/HOME.README.md ~/README.md
 echo "[ -f $HOME/dot/bash/entry.bashrc ] && source $HOME/dot/bash/entry.bashrc" >> ~/.bashrc
+
+# on OSX, bash does not read ~/.bashrc by default, but it does read ~/.profile
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	ln -s ~/.bashrc ~/.profile
+fi
