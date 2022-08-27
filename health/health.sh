2b67e2381d1f045e0e465fa1d34b9c8fcc35cb35
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-08-27 01:37:37 -0500

    rename health-s to health-files.sh

--- health/health.sh
+++ health/health.sh
@@ -1,3 +1,3 @@
 ~/dot/health/health-dot.sh | grep "^\[!!\]"
-~/dot/health/health-s.sh | grep "^\[!!\]"
+~/dot/health/health-files.sh | grep "^\[!!\]"
 ~/dot/health/health-syncthing.sh | grep "^\[!!\]"
