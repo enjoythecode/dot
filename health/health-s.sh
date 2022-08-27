801b1b4506aaaaf1d50d1a994b3e82195c03d184
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2022-08-27 01:37:06 -0500

    add checking of downloads to health-s.sh

--- health/health-s.sh
+++ health/health-s.sh
@@ -2,3 +2,12 @@ find "$S_PATH_TOSORT" -type f \( ! -iname ".DS_Store" \) |
 while IFS= read -r line;do
 	echo "[!!] Unsorted file at $line"
 done
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+	find "$HOME/Downloads" -type f \( ! -iname ".DS_Store" \) |
+	while IFS= read -r line;do
+		echo "[!!] Unsorted download at $line"
+	done
+else
+	echo "[!!] Downloads and Desktop folder paths not defined for non-mac OSs yet!"
+fi
