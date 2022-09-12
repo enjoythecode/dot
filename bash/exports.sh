b126dd169ab2c9bfc27bb250ee4d5e341cf47061
Sinan Yumurtaci <sinan.yumurtaci@gmail.com>
2022-09-11 20:40:00 -0400

    fix bash exports for paths in WSL under Windows

--- bash/exports.sh
+++ bash/exports.sh
@@ -15,13 +15,26 @@ export PATH=$PATH:~/apache/apache-maven-3.8.5/bin # apache should probably not b
 
 export CUSTOM_BASHRC_SOURCED="true"
 
-export S_PATH_S="$HOME/S"
-export S_PATH_BEANS="$HOME/S/beans"
-export S_PATH_ARCHIVE="$HOME/S/Archive"
-export S_PATH_TOSORT="$HOME/S/Archive/ToSort"
-export S_PATH_CURRENT="$HOME/S/Current"
-export S_PATH_DOT="$HOME/dot"
+# Check if we are running in WSL under Windows, in which case we have a specific "Home" where S/ etc. are stored,
+# separate from "Home" as in tilda (~)
+# Source: https://stackoverflow.com/questions/60922620/shell-script-to-check-if-running-in-windows-when-using-wsl
+if [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ];
+then
+	export S_PATH_HOME="/mnt/c/Users/sinan/"
+else
+	export S_PATH_HOME="$HOME"
+fi
+
+# Paths that derive from "Home", wherever that is
+export S_PATH_S="$S_PATH_HOME/S"
+export S_PATH_BEANS="$S_PATH_HOME/S/beans"
+export S_PATH_ARCHIVE="$S_PATH_HOME/S/Archive"
+export S_PATH_TOSORT="$S_PATH_HOME/S/Archive/ToSort"
+export S_PATH_BASHRC="$S_PATH_HOME/.bashrc"
+export S_PATH_CURRENT="$S_PATH_HOME/S/Current"
+export S_PATH_DOT="$S_PATH_HOME/dot"
+
+# Paths that derive from the above (ie, indirectly dependent on "Home")
 export S_PATH_WIKI="$S_PATH_S/Zettelkasten"
 export S_PATH_VIMRC="$S_PATH_DOT/nvim/init.vim"
-export S_PATH_BASHRC="$HOME/.bashrc"
 export S_PATH_BASH_ALIASES="$S_PATH_DOT/bash/aliases_and_functions.sh"
