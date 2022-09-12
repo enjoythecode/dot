69562e3c78292ca0270b04210104b7014b025e41
enjoythecode <sinan.yumurtaci@gmail.com>
2022-09-11 21:02:33 -0400

    stricter correctness in bash/exports (more quotes)

--- bash/exports.sh
+++ bash/exports.sh
@@ -5,13 +5,13 @@ else
     export EDITOR="vim"
 fi
 
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
 
-export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin # eventually obsolete
-export PATH=$PATH:/usr/local/share/dotnet # what is this?
-export PATH=$PATH:/Users/sinany/bin
-export PATH=$PATH:~/apache/apache-maven-3.8.5/bin # apache should probably not be in ~, but who am i to say?
+export PATH="$PATH:/Library/Frameworks/Python.framework/Versions/3.7/bin" #eventually obsolete
+export PATH="$PATH:/usr/local/share/dotnet" # what is this?
+export PATH="$PATH:/Users/sinany/bin"
+export PATH="$PATH:~/apache/apache-maven-3.8.5/bin" # apache should probably not be in ~, but who am i to say?
 
 export CUSTOM_BASHRC_SOURCED="true"
 
