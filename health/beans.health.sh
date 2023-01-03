149d865e210956a000b4cc366b012f754438459b
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2023-01-03 13:18:53 -0600

    add beans health check and fix health script output interfering with each other

---
+++ health/beans.health.sh
@@ -0,0 +1,1 @@
+cd ~/S/beans/ && make check
--- health/health.sh
+++ health/health.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function find_and_run_health_script_in_directory() {
-	find $1 -name "*.health.sh" | xargs /bin/bash
+	find $1 -name "*.health.sh" | xargs -L 1 /bin/bash
 }
 
 find_and_run_health_script_in_directory ~/dot
