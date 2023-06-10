448ae2472bc42d9275c6fd200a5326f0c69196fb
Sinan Yumurtaci <sinan.yumurtaci@gmail.com>
2023-06-10 00:52:53 -0400

    parametrize more of the paths in bash/link.sh

--- sh/link.sh
+++ sh/link.sh
@@ -4,10 +4,12 @@ source ~/dot/bash/exports.sh
 
 echo "$S_PATH_HOME"
 
-ln -s ~/dot/nvim ~/.config/
-ln -s ~/dot/sinany/docs/HOME.README.md ~/README.md
-ln -s ~/dot/sinany/docs/HOME.TODO.md ~/TODO.md
-echo "[ -f ~/dot/bash/entry.sh ] && source ~/dot/bash/entry.sh" >> ~/.bashrc
+ln -s "$S_PATH_DOT/nvim" ~/.config/
+ln -s "$S_PATH_DOT/sinany/docs/HOME.README.md" ~/README.md
+ln -s "$S_PATH_DOT/sinany/docs/HOME.TODO.md" ~/TODO.md
+# TODO: parametrize the location of dot in the following line also (ie
+# $S_PATH_DOT)
+echo [ -f ~/dot/bash/entry.sh ] && source ~/dot/bash/entry.sh >> ~/.bashrc
 
 # on OSX, bash does not read ~/.bashrc by default, but it does read ~/.profile
 if [[ "$OSTYPE" == "darwin"* ]]; then
