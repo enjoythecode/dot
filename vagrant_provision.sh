339d59f88b7b9618d90057595d56ee254c675bed
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2023-08-07 04:46:51 +0000

    organize vagrant provision sh

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -2,12 +2,16 @@ sudo dnf install -y neovim python3-neovim
 sudo dnf install -y ripgrep
 sudo dnf install -y fzf
 sudo dnf install -y python3-pip
+
 git clone https://github.com/enjoythecode/dot.git /home/vagrant/dot
 mkdir /home/vagrant/.config
 ln -s /home/vagrant/dot/nvim /home/vagrant/.config/nvim
 ln -s /home/vagrant/dot/bash /home/vagrant/.bashrc.d
+
+git config pull.rebase true
 git config --global user.email "sinan.yumurtaci@gmail.com"
 git config --global user.name "Sinan Yumurtacı"
+
 python3 -m pip install neovim
 # this one does not yet work, for some reason...
 # python3 -m pip install beancount
