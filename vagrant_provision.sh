053217e6992f827f374f4db5ea18966994332a5e
Sinan Yumurtacı <syumurtaci@broadwaytechnology.com>
2023-08-09 09:47:45 -0500

    fix vagrant_provision.sh

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -3,11 +3,16 @@ sudo dnf install -y ripgrep
 sudo dnf install -y fzf
 sudo dnf install -y python3-pip
 
-git clone git@github.com:enjoythecode/dot.git /home/vagrant/dot
+# clone with HTTPS and then switch to SSH because I am lazy and 
+# don't want to figure out how to automatically add the GitHub host
+# to my trusted SSH prints.
+git clone https://github.com/enjoythecode/dot.git /home/vagrant/dot
+(cd dot && git remote set-url origin git@github.com:enjoythecode/dot.git)
+
 mkdir /home/vagrant/.config
 ln -s /home/vagrant/dot/nvim /home/vagrant/.config/nvim
 mkdir /home/vagrant/.bashrc.d
-ln -s /home/vagrant/dot/bash /home/vagrant/.bashrc.d/entry.sh
+ln -s /home/vagrant/dot/bash/entry.sh /home/vagrant/.bashrc.d/entry.sh
 
 git config pull.rebase true
 git config --global user.email "sinan.yumurtaci@gmail.com"
