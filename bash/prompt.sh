1d072c443fa6abe8aea80706452f49c567084c1f
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2023-08-09 05:40:02 +0000

    clean up prompt; remove timer and status code

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,47 +1,3 @@
-function timer_now {
-    if [[ "$OSTYPE" == "darwin"* ]];
-    then
-        if command -v gdate &> /dev/null
-        then
-            gdate +%s%N
-        else
-            date +%s # less accurate, but still works
-        fi
-    else
-        date +%s
-    fi
-}
-
-function timer_start {
-    timer_start=${timer_start:-$(timer_now)}
-}
-
-function timer_stop {
-    local delta_us=$((($(timer_now) - $timer_start) / 1000))
-    local us=$((delta_us % 1000))
-    local ms=$(((delta_us / 1000) % 1000))
-    local s=$(((delta_us / 1000000) % 60))
-    local m=$(((delta_us / 60000000) % 60))
-    local h=$((delta_us / 3600000000))
-    # Goal: always show around 3 digits of accuracy
-    if ((h > 0)); then timer_show=${h}h${m}m
-    elif ((m > 0)); then timer_show=${m}m${s}s
-    elif ((s >= 10)); then timer_show=${s}.$((ms / 100))s
-    elif ((s > 0)); then timer_show=${s}.$(printf %03d $ms)s
-    elif ((ms >= 100)); then timer_show=${ms}ms
-    elif ((ms > 0)); then timer_show=${ms}.$((us / 100))ms
-    else timer_show=${us}us
-    fi
-	# if command runs for longer than 10 seconds, send a notification
-	# this has the added benefit of telling me how long I was in nvim for, which
-	# should be an interesting anecdote to collect and give me some mindfullness
-	# around how much time I spend...
-	if ((s >= 10)); then cs_send_notification "Your task just ended. That took $timer_show."
-	fi
-    unset timer_start
-}
-
-
 set_prompt () {
     Last_Command=$? # Must come first!
     Blue='\[\e[01;34m\]'
@@ -52,9 +8,7 @@ set_prompt () {
     FancyX='\342\234\227'
     Checkmark='\342\234\223'
 
-
-    # Add a bright white exit status for the last command
-    PS1="$White\$? "
+	PS1=""
     # If it was successful, print a green check mark. Otherwise, print
     # a red X.
     if [[ $Last_Command == 0 ]]; then
@@ -62,10 +16,6 @@ set_prompt () {
     else
         PS1+="$Red$FancyX "
     fi
-
-    # Add the ellapsed time and current date
-    timer_stop
-    PS1+="($timer_show) \t "
 
     # If root, just print the host in red. Otherwise, print the current user
     # and host in green.
@@ -87,6 +37,5 @@ set_prompt () {
 	PS1+="$Blue\\\$$Reset "
 }
 
-trap 'timer_start' DEBUG
 PROMPT_COMMAND='set_prompt'
 
