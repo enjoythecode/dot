407736204881d4e6e08eb51ef6c08214d1c00f14
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2024-02-02 04:53:48 +0000

    add error checking to beancount.health, remove todo from h output

--- health/beans.health.sh
+++ health/beans.health.sh
@@ -1,3 +1,11 @@
-cd "$S_PATH_S/beans" && make check
 
-echo "[i ] Ran checks for beancount"
+cd "$S_PATH_S/beans" && make check
+# todo(sy) what is the return value of a && statement in bash? am I actually
+# catching the correct error code?
+Last_Command=$? # Must come right after the check!
+
+if [[ $Last_Command == 0 ]]; then
+    echo "[OK] Beancount is healthy"
+else
+    echo "[!!] Beancount reported issues"
+fi
