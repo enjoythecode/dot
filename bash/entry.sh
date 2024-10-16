bb9b6f6354c497a356ccead0fee03a0ab78750cc
Sinan Yumurtacı <sinan.yumurtaci@gmail.com>
2024-10-16 00:05:13 -0500

    easy fix

--- bash/entry.sh
+++ bash/entry.sh
@@ -1,5 +1,5 @@
-. /home/vagrant/dot/bash/configuration.sh
-. /home/vagrant/dot/bash/exports.sh
-. /home/vagrant/dot/bash/aliases_and_functions.sh
-. /home/vagrant/dot/bash/prompt.sh
-. /home/vagrant/dot/bash/z.sh
+. ~/dot/bash/configuration.sh
+. ~/dot/bash/exports.sh
+. ~/dot/bash/aliases_and_functions.sh
+. ~/dot/bash/prompt.sh
+. ~/dot/bash/z.sh
